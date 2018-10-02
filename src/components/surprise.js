04317c666e93a62ea4f7d47ff0280e47bc69d041
Glitch (react-surprise-starting-point) <none>
2018-10-02 15:42:59 +0000

    💸🎏 Checkpoint ./src/components/surprise.js:1725100/446

--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,10 +6,14 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+        this.state ={
+          currentGif: ''
+        }
     }
 
     render() {
         // Show the button to start with
         return <SurpriseButton />;
+  
     }
 }
